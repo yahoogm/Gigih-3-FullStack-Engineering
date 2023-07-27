8f6fc9066606b01f1bffb5d16d32d455a2de40c9
yahoogm <yaho.garfield@gmail.com>
2023-07-27 15:56:01 +0700

    add module 3

---
+++ module-3/session-1/script.js
@@ -0,0 +1,170 @@
+const db = connect('mongodb://localhost:27017/playlists');
+
+db.songs.insertMany([
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+  {
+    title: 'BYOB',
+    name: 'System Of A Down',
+    album: 'BYOB',
+  },
+]);
+
+db.artist.insertMany([
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+  {
+    name: 'System Of A Down',
+    dateOfBirth: '1994',
+    genre: ['Meta,Rock'],
+  },
+]);
+
+db.popularSongs.insertMany([
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+  {
+    title: 'BYOB',
+    played: 1000,
+    period: '3.12',
+  },
+]);
